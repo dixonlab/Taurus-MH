aa25869d8f70a133afa8201a831f4cfb1703165c
Dongsung Lee <eaststar0@gmail.com>
2019-05-03 01:22:09 -0700

    Update Trimming.py

--- Trimming.py
+++ Trimming.py
@@ -2,8 +2,8 @@ import sys
 import gzip
 dfh1=gzip.open(sys.argv[1],'r')
 dfh2=gzip.open(sys.argv[1].replace('R1','R2'),'r')
-rfh1=open(sys.argv[1].split('/')[-1]+'_F25_L4_trimmed.fastq','w')
-rfh2=open(sys.argv[1].split('/')[-1].replace('R1','R2')+'_F25_L4_trimmed.fastq','w')
+rfh1=open(sys.argv[1].split('/')[-1]+'_F25_L3_trimmed.fastq','w')
+rfh2=open(sys.argv[1].split('/')[-1].replace('R1','R2')+'_F25_L3_trimmed.fastq','w')
 line1_1=dfh1.readline()
 line1_2=dfh1.readline()
 line1_3=dfh1.readline()
@@ -14,9 +14,9 @@ line2_2=dfh2.readline()
 line2_3=dfh2.readline()
 line2_4=dfh2.readline()
 while line1_1:
-        if len(line1_2[25:-4])>=50 and line2_2[25:-4]>=50:
-                rfh1.write(line1_1+line1_2[25:-4]+'\n'+line1_3+line1_4[25:-4]+'\n')
-                rfh2.write(line2_1+line2_2[25:-4]+'\n'+line2_3+line2_4[25:-4]+'\n')
+        if len(line1_2[25:-3])>=50 and line2_2[25:-3]>=50:
+                rfh1.write(line1_1+line1_2[25:-3]+'\n'+line1_3+line1_4[25:-3]+'\n')
+                rfh2.write(line2_1+line2_2[25:-3]+'\n'+line2_3+line2_4[25:-3]+'\n')
         line1_1=dfh1.readline()
         line1_2=dfh1.readline()
         line1_3=dfh1.readline()
@@ -25,9 +25,9 @@ while line1_1:
         line2_2=dfh2.readline()
         line2_3=dfh2.readline()
         line2_4=dfh2.readline()
-if len(line1_2[25:-4])>=50 and line2_2[25:-4]>=50:                                  
-        rfh1.write(line1_1+line1_2[25:-4]+'\n'+line1_3+line1_4[25:-4]+'\n')
-        rfh2.write(line2_1+line2_2[25:-4]+'\n'+line2_3+line2_4[25:-4]+'\n')
+if len(line1_2[25:-3])>=50 and line2_2[25:-3]>=50:                                  
+        rfh1.write(line1_1+line1_2[25:-3]+'\n'+line1_3+line1_4[25:-3]+'\n')
+        rfh2.write(line2_1+line2_2[25:-3]+'\n'+line2_3+line2_4[25:-3]+'\n')
 rfh1.close()
 dfh1.close()
 rfh2.close()
