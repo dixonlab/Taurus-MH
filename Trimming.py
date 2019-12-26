73619b88b1eb6539334db98649e9ac2c53ce5bc7
Dongsung Lee <eaststar0@gmail.com>
2019-12-25 21:16:03 -0800

    Update Trimming.py

--- Trimming.py
+++ Trimming.py
@@ -15,10 +15,16 @@ line2_1=dfh2.readline()
 line2_2=dfh2.readline()
 line2_3=dfh2.readline()
 line2_4=dfh2.readline()
+
 while line1_1:
-        if len(line1_2[F_trim:-1*B_trim])>=50 and line2_2[F_trim:-1*B_trim]>=50:
-                rfh1.write(line1_1+line1_2[F_trim:-1*B_trim]+'\n'+line1_3+line1_4[F_trim:-1*B_trim]+'\n')
-                rfh2.write(line2_1+line2_2[F_trim:-1*B_trim]+'\n'+line2_3+line2_4[F_trim:-1*B_trim]+'\n')
+        if B_trim!=0:
+                if len(line1_2[F_trim:-1*B_trim])>=50 and line2_2[F_trim:-1*B_trim]>=50:
+                        rfh1.write(line1_1+line1_2[F_trim:-1*B_trim]+'\n'+line1_3+line1_4[F_trim:-1*B_trim]+'\n')
+                        rfh2.write(line2_1+line2_2[F_trim:-1*B_trim]+'\n'+line2_3+line2_4[F_trim:-1*B_trim]+'\n')
+        if B_trim==0:
+                if len(line1_2[F_trim:])>=50 and line2_2[F_trim:]>=50:
+                        rfh1.write(line1_1+line1_2[F_trim:]+'\n'+line1_3+line1_4[F_trim:]+'\n')
+                        rfh2.write(line2_1+line2_2[F_trim:]+'\n'+line2_3+line2_4[F_trim:]+'\n')
         line1_1=dfh1.readline()
         line1_2=dfh1.readline()
         line1_3=dfh1.readline()
@@ -27,9 +33,14 @@ while line1_1:
         line2_2=dfh2.readline()
         line2_3=dfh2.readline()
         line2_4=dfh2.readline()
-if len(line1_2[F_trim:-1*B_trim])>=50 and line2_2[F_trim:-1*B_trim]>=50:                                  
-        rfh1.write(line1_1+line1_2[F_trim:-1*B_trim]+'\n'+line1_3+line1_4[F_trim:-1*B_trim]+'\n')
-        rfh2.write(line2_1+line2_2[F_trim:-1*B_trim]+'\n'+line2_3+line2_4[F_trim:-1*B_trim]+'\n')
+if B_trim!=0:
+        if len(line1_2[F_trim:-1*B_trim])>=50 and line2_2[F_trim:-1*B_trim]>=50:                                  
+                rfh1.write(line1_1+line1_2[F_trim:-1*B_trim]+'\n'+line1_3+line1_4[F_trim:-1*B_trim]+'\n')
+                rfh2.write(line2_1+line2_2[F_trim:-1*B_trim]+'\n'+line2_3+line2_4[F_trim:-1*B_trim]+'\n')
+if B_trim==0:
+        if len(line1_2[F_trim:])>=50 and line2_2[F_trim:]>=50:
+                rfh1.write(line1_1+line1_2[F_trim:]+'\n'+line1_3+line1_4[F_trim:]+'\n')
+                rfh2.write(line2_1+line2_2[F_trim:]+'\n'+line2_3+line2_4[F_trim:]+'\n')
 rfh1.close()
 dfh1.close()
 rfh2.close()
